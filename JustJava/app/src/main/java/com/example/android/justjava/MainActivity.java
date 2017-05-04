c052551250e4e7bc645ba74e886af60f93dcdf18
slushee <tylerpham.9@gmail.com>
2017-05-03 18:43:33 -0700

    Name Field
    Customizes the order to show the user's name

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -9,11 +9,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
 import java.text.NumberFormat;
+
+import static android.R.id.edit;
+
 /**
  * This app displays an order form to order coffee.
  */
@@ -31,11 +35,18 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
+        EditText nameField = (EditText) findViewById(R.id.name_field);
+        String name = nameField.getText().toString();
+
         CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
         boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+
         CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
         boolean hasChocolate = chocolateCheckBox.isChecked();
-        displayMessage(createOrderSummary(hasWhippedCream, hasChocolate));
+
+
+
+        displayMessage(createOrderSummary(name, hasWhippedCream, hasChocolate));
     }
 
     public void increment(View view) {
@@ -68,8 +79,8 @@ public class MainActivity extends AppCompatActivity {
         int price = quantity*pricePerCup;
         return price;
     }
-    private String createOrderSummary(boolean addWhippedCream, boolean addChocolate){
-        String priceMessage = "Name: Kaptain Kunal";
+    private String createOrderSummary(String name, boolean addWhippedCream, boolean addChocolate){
+        String priceMessage = "Name: " + name;
         priceMessage += "\nAdd Whipped Cream? " + addWhippedCream;
         priceMessage += "\nAdd Chocolate? " + addChocolate;
         priceMessage += "\nQuantity: " + quantity;
